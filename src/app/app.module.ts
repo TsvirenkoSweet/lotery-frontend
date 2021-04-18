f998b691394ecabf3d4334e25852b9eea9428edd
Andrei Tsvirenko <andrei.tsvirenko@softwaredesign.ie>
2021-04-18 22:05:23 +0300

    basic setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductComponent } from './product/product.component';
+import { AuthComponent } from './auth/auth.component';
+import { AccountComponent } from './account/account.component';
+import { FooterComponent } from './footer/footer.component';
+import { BillingComponent } from './billing/billing.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+import { AdminProductComponent } from './admin-product/admin-product.component';
+import { AdminReportComponent } from './admin-report/admin-report.component';
+import { AdminUserComponent } from './admin-user/admin-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductsComponent,
+    ProductComponent,
+    AuthComponent,
+    AccountComponent,
+    FooterComponent,
+    BillingComponent,
+    NotFoundComponent,
+    AdminPanelComponent,
+    AdminProductComponent,
+    AdminReportComponent,
+    AdminUserComponent,
   ],
   imports: [
     BrowserModule,
